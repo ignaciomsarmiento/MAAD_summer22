ca09fc401dcff77fff4a7a363dc0e56cae137d0c
Ignacio Sarmiento-Barbieri <ignaciomsarmiento@gmail.com>
2022-06-23 20:59:43 -0500

    Update tips.R

--- ProblemSet1/tips.R
+++ ProblemSet1/tips.R
@@ -8,18 +8,30 @@ dta<-read_csv("/Users/iggy/Dropbox/Teaching/2022/SpatialDataEng/Lectures/Problem
 
 map<-st_read("/Users/iggy/Dropbox/Teaching/2022/SpatialDataEng/Lectures/ProblemSet1/data/Areas_comunitarias_Chicago/geo_export_504ee058-3b12-4c1e-8fa8-a920109f7ad2.shp")
 
-dta_sf<-st_as_sf(dta,coords=c("lat","lon"),remove=FALSE,crs=4326)
+dta_sf<-st_as_sf(dta,coords=c("lon","lat"),remove=FALSE,crs=4326)
 
 require("leaflet")
 leaflet(dta_sf[1:10,]) %>% 
   addTiles() %>% 
   addMarkers()
 
-
+dta_sf<-st_transform(dta_sf,st_crs(map))
 db<-st_join(dta_sf,map,join=st_intersects)
 
 
+db_group<- db %>% st_drop_geometry() %>% group_by(community) %>% summarize(n=n())
+
+map<- left_join(map,db_group)
+map<- map %>% mutate(logn=log())
+ggplot() +
+  geom_sf(data=map,aes(fill=n)) +
+  theme_bw()
 
 
 
-#dta_sf<-st_transform(dta_sf,st_crs(map))
+
+
+
+
+
+#
